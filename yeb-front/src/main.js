e445a97af0b5b35c8c406aa94c734e078f494f97
OneDayInMarch <2511437719@qq.com>
2022-06-11 14:19:32 +0800

    登陆页面设计

---
+++ yeb-front/src/main.js
@@ -0,0 +1,19 @@
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'// 设置反向代理，前端请求默认发送到 http://localhost:8081/api
+var axios = require('axios')
+axios.defaults.baseURL = 'http://localhost:8081/api'
+// 全局注册，之后可在其他组件中通过 this.$axios 发送数据
+Vue.prototype.$axios = axios
+Vue.config.productionTip = false
+Vue.use(ElementUI);
+
+/* eslint-disable no-new */
+new Vue({
+el: '#app',
+router,
+components: { App },
+template: '<App/>'
+})
---
+++ yeb-front/src/router/index.js
@@ -0,0 +1,16 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import Login from "@/views/Login";
+ 
+Vue.use(Router)
+ 
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      name: 'Login',
+      component: Login,
+      hidden: true // 不会被循环遍历出来
+  },
+  ]
+})
