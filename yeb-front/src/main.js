3295cae2f4d15b0c95dd46e23b3d820013c95949
OneDayInMarch <2511437719@qq.com>
2022-06-22 22:21:57 +0800

    初始化菜单

--- yeb-front/src/main.js
+++ yeb-front/src/main.js
@@ -1,19 +1,58 @@
 import Vue from 'vue'
-import App from './App'
+import App from './App.vue'
 import router from './router'
-import ElementUI from 'element-ui'
-import 'element-ui/lib/theme-chalk/index.css'// 设置反向代理，前端请求默认发送到 http://localhost:8081/api
-var axios = require('axios')
-axios.defaults.baseURL = 'http://localhost:8081/api'
-// 全局注册，之后可在其他组件中通过 this.$axios 发送数据
-Vue.prototype.$axios = axios
-Vue.config.productionTip = false
-Vue.use(ElementUI);
+import store from './store'
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+import 'font-awesome/css/font-awesome.min.css'
 
-/* eslint-disable no-new */
-new Vue({
-el: '#app',
-router,
-components: { App },
-template: '<App/>'
+import {postRequest} from "@/utils/api";
+import {putRequest} from "@/utils/api";
+import {getRequest} from "@/utils/api";
+import {deleteRequest} from "@/utils/api";
+import {initMenu} from "@/utils/menus";
+// import {downloadRequest} from "@/utils/download";
+
+Vue.config.productionTip = false
+Vue.use(ElementUI,{size:'small'});
+// 插件形式使用请求
+Vue.prototype.postRequest = postRequest
+Vue.prototype.putRequest = putRequest
+Vue.prototype.getRequest = getRequest
+Vue.prototype.deleteRequest = deleteRequest
+// Vue.prototype.downloadRequest = downloadRequest // 以插件的形式使用下载相关请求
+
+// 使用 router.beforeEach 注册一个全局前置守卫
+router.beforeEach((to, from, next) => {
+    // to 要去的路由; from 来自哪里的路由 ; next() 放行
+    // 用户登录成功时，把 token 存入 sessionStorage，如果携带 token，初始化菜单，放行
+    if (window.sessionStorage.getItem('tokenStr')) {
+        initMenu(router, store)
+        // 如果用户不存在
+        if (!window.sessionStorage.getItem('user')) {
+            // 判断用户信息是否存在
+            return getRequest('/admin/info').then(resp => {
+                if (resp) {
+                    // 存入用户信息，转字符串，存入 sessionStorage
+                    window.sessionStorage.setItem('user', JSON.stringify(resp))
+                    // 同步用户信息 编辑用户
+                    store.commit('INIT_ADMIN',resp)
+                    next();
+                }
+            })
+        }
+        next();
+    } else {
+        if (to.path === '/') {
+            next()
+        } else {
+            next('/?redirect=' + to.path)
+        }
+    }
 })
+
+new Vue({
+    router,
+    store,
+    render: h => h(App)
+}).$mount('#app')
--- yeb-front/src/router/index.js
+++ yeb-front/src/router/index.js
@@ -1,16 +1,40 @@
 import Vue from 'vue'
-import Router from 'vue-router'
+import VueRouter from 'vue-router'
 import Login from "@/views/Login";
- 
-Vue.use(Router)
- 
-export default new Router({
-  routes: [
+import Home from "@/views/Home";
+// import FriendChat from "@/views/chat/FriendChat";
+// import AdminInfo from "@/views/AdminInfo";
+
+Vue.use(VueRouter)
+
+const routes = [
     {
-      path: '/',
-      name: 'Login',
-      component: Login,
-      hidden: true // 不会被循环遍历出来
-  },
-  ]
+        path: '/',
+        name: 'Login',
+        component: Login,
+        hidden: true // 不会被循环遍历出来
+    },
+    {
+        path: '/home',
+        name: 'Home',
+        component: Home,
+        // children: [
+        //     {
+        //         path: '/chat',
+        //         name: '在线聊天',
+        //         component: FriendChat
+        //     },
+        //     {
+        //         path: '/userinfo',
+        //         name: '个人中心',
+        //         component: AdminInfo
+        //     }
+        // ]
+    }
+]
+
+const router = new VueRouter({
+    routes
 })
+
+export default router
